f1c731bc5e1b07525d816a848f3f94037b31de6f
Roman Deriga <deriga.roman@gmail.com>
2023-08-07 11:28:15 +0300

    Add list comprehansion

--- main.py
+++ main.py
@@ -34,27 +34,25 @@ def main():
     place = input('Введите ваше местопорожение: ')
     place_longitude, place_latitude = fetch_coordinates(YANDEX_API_KEY, place)
     print(f'Ваши координаты: {place_longitude, place_latitude}')
-    coffee_shops_with_distance = []
+
     with open('coffee.json', 'r') as file:
         content = file.read()
         coffee_shops = json.loads(content)
-        for cafe in coffee_shops:
-            item = {
-                'name': cafe['Name'],
-                'longitude': cafe['Longitude_WGS84'],
-                'latitude': cafe['Latitude_WGS84'],
-                'distance': distance.distance(
-                    (place_latitude, place_longitude),
-                    (cafe['Latitude_WGS84'], cafe['Longitude_WGS84'])
-                ).km
-            }
-            coffee_shops_with_distance.append(item)
-        sorted_cafes = sorted(coffee_shops_with_distance, key=get_distance_to_cafe)
-        five_nearest_cafe = sorted_cafes[:5]
-        pprint.pprint(five_nearest_cafe, sort_dicts=False)
-        nearest_cafe = min(coffee_shops_with_distance, key=get_distance_to_cafe)
-        #pprint.pprint(nearest_cafe, sort_dicts=False)
-        #pprint.pprint(coffee_shops_with_distance, sort_dicts=False)
+        
+    coffee_shops_with_distance = [{
+        'name': cafe['Name'],
+        'longitude': cafe['Longitude_WGS84'],
+        'latitude': cafe['Latitude_WGS84'],
+        'distance': distance.distance(
+            (place_latitude, place_longitude),
+            (cafe['Latitude_WGS84'], cafe['Longitude_WGS84'])).km,
+        }
+        for cafe in coffee_shops]
+
+    sorted_coffee_shops = sorted(coffee_shops_with_distance, key=get_distance_to_cafe)
+    five_nearest_coffee_shops = sorted_coffee_shops[:5]
+    nearest_cafe = min(coffee_shops_with_distance, key=get_distance_to_cafe)
+    #pprint.pprint(nearest_cafe, sort_dicts=False)
 
 
 if __name__ == '__main__':

06092e37b942775714ef2880ef76e0c464677e73
Roman Deriga <deriga.roman@gmail.com>
2023-08-07 13:22:24 +0300

    Add flask

--- main.py
+++ main.py
@@ -3,33 +3,26 @@ import pprint
 import folium
 from geopy import distance
 from dotenv import dotenv_values
-import requests
+from flask import Flask
+
+from fetch_coordinates import fetch_coordinates
 
 
 YANDEX_API_KEY = dotenv_values('.env')['YANDEX_API_KEY']
 
 
 def gen_map(place, coffee_shops):
-    m = folium.Map(location=[place])
+    m = folium.Map(location=place, zoom_start=13)
+    
+    for coffee_shop in coffee_shops:
+        folium.Marker(
+            [coffee_shop['latitude'],
+             coffee_shop['longitude']
+            ],
+            tooltip=coffee_shop['name']
+            ).add_to(m)
+
     m.save('index.html')
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
 
 
 def get_distance_to_cafe(cafe):
@@ -40,7 +33,7 @@ def main():
     place = input('Введите ваше местопорожение: ')
     place_longitude, place_latitude = fetch_coordinates(YANDEX_API_KEY, place)
     print(f'Ваши координаты: {place_longitude, place_latitude}')
-
+    
     with open('coffee.json', 'r') as file:
         content = file.read()
         coffee_shops = json.loads(content)
@@ -57,9 +50,17 @@ def main():
 
     sorted_coffee_shops = sorted(coffee_shops_with_distance, key=get_distance_to_cafe)
     five_nearest_coffee_shops = sorted_coffee_shops[:5]
-    nearest_cafe = min(coffee_shops_with_distance, key=get_distance_to_cafe)
-    #pprint.pprint(nearest_cafe, sort_dicts=False)
+
+    gen_map([place_latitude, place_longitude], five_nearest_coffee_shops)
+
+
+def run_site():
+    with open('index.html', 'r') as file:
+        return file.read()
 
 
 if __name__ == '__main__':
     main()
+    app = Flask(__name__)
+    app.add_url_rule('/', 'Test page', run_site)
+    app.run('0.0.0.0')
